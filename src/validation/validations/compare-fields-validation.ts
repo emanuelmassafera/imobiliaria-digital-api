a1742cae304402b49e95106fc1e4956efe1dea6a
Emanuel Massafera <emanuel301@live.com>
2021-04-24 13:33:26 -0300

    feat: create CompareFieldsValidation

---
+++ src/validation/validations/compare-fields-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '@/domain/errors'
+import { Validation } from '@/presentation/protocols'
+
+export class CompareFieldsValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldToCompareName: string
+  ) {}
+
+  validate (input: any): Error {
+    if (input[this.fieldName] && input[this.fieldToCompareName]) {
+      if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+        return new InvalidParamError(this.fieldToCompareName)
+      }
+    }
+  }
+}
--- src/validation/validations/index.ts
+++ src/validation/validations/index.ts
@@ -1,3 +1,4 @@
+export * from './compare-fields-validation'
 export * from './cpf-validation'
 export * from './email-validation'
 export * from './int-validation'
