23bb1bc79e26b0be85fa52663079b70fd07ad787
Emanuel Massafera <emanuel301@live.com>
2021-04-24 13:46:18 -0300

    feat: create StrongPasswordValidation

---
+++ src/validation/validations/strong-password-validation.ts
@@ -0,0 +1,19 @@
+import { InvalidParamError } from '@/domain/errors'
+import { Validation } from '@/presentation/protocols'
+import { StrongPasswordValidator } from '@/validation/protocols'
+
+export class StrongPasswordValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly strongPasswordValidator: StrongPasswordValidator
+  ) {}
+
+  validate (input: any): Error {
+    if (input[this.fieldName]) {
+      const isValid = this.strongPasswordValidator.isStrongPassword(String(input[this.fieldName]))
+      if (!isValid) {
+        return new InvalidParamError(this.fieldName)
+      }
+    }
+  }
+}
