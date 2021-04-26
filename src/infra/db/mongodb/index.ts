70aeba15a1e91d2481d255c55acbb0bb64f2d4cd
Emanuel Massafera <emanuel301@live.com>
2021-04-25 22:02:37 -0300

    feat: create LogControllerDecorator

--- src/infra/db/mongodb/index.ts
+++ src/infra/db/mongodb/index.ts
@@ -1,2 +1,3 @@
+export * from './log-mongo-repository'
 export * from './mongo-helper'
 export * from './owner-mongo-repository'
--- tests/data/mocks/index.ts
+++ tests/data/mocks/index.ts
@@ -1,2 +1,3 @@
 export * from './mock-cryptography'
+export * from './mock-db-log'
 export * from './mock-db-owner'
