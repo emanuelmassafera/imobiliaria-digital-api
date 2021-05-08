e131f833be4f2c868f04b5ce6ea05160838e695c
Emanuel Massafera <emanuel301@live.com>
2021-05-08 13:26:57 -0300

    feat: add loadProperties method to PropertyMongoRepository

--- src/infra/db/mongodb/property-mongo-repository.ts
+++ src/infra/db/mongodb/property-mongo-repository.ts
@@ -1,11 +1,68 @@
-import { AddPropertyRepository } from '@/data/protocols/db'
-import { AddProperty } from '@/domain/usecases'
-import { MongoHelper } from '@/infra/db'
+import { AddPropertyRepository, LoadPropertiesRepository } from '@/data/protocols/db'
+import { MongoHelper, QueryBuilder } from '@/infra/db'
 
-export class PropertyMongoRepository implements AddPropertyRepository {
-  async add (params: AddProperty.Params): Promise<AddPropertyRepository.Result> {
+import { ObjectId } from 'mongodb'
+
+export class PropertyMongoRepository implements AddPropertyRepository, LoadPropertiesRepository {
+  async add (params: AddPropertyRepository.Params): Promise<AddPropertyRepository.Result> {
     const propertyCollection = await MongoHelper.getCollection('properties')
     const result = await propertyCollection.insertOne(params)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async loadProperties (params: LoadPropertiesRepository.Params): Promise<LoadPropertiesRepository.Result> {
+    const propertyCollection = await MongoHelper.getCollection('properties')
+    const queryBuilder = new QueryBuilder()
+
+    if (params.ownerId) {
+      queryBuilder.match({
+        ownerId: new ObjectId(params.ownerId)
+      })
+    }
+
+    if (params.type) {
+      queryBuilder.match({
+        type: params.type
+      })
+    }
+
+    if (params.availableTo) {
+      queryBuilder.match({
+        availableTo: params.availableTo
+      })
+    }
+
+    if (params.state) {
+      queryBuilder.match({
+        'address.state': { $regex: params.state, $options: 'i' }
+      })
+    }
+
+    if (params.city) {
+      queryBuilder.match({
+        'address.city': { $regex: params.city, $options: 'i' }
+      })
+    }
+
+    if (params.neighborhood) {
+      queryBuilder.match({
+        'address.neighborhood': { $regex: params.neighborhood, $options: 'i' }
+      })
+    }
+
+    if (params.minimumOfBedrooms) {
+      queryBuilder.match({
+        numberOfBedrooms: { $gte: params.minimumOfBedrooms }
+      })
+    }
+
+    if (params.active) {
+      queryBuilder.match({
+        status: 'active'
+      })
+    }
+
+    const properties = await propertyCollection.aggregate(queryBuilder.query).toArray()
+    return MongoHelper.mapCollection(properties)
   }
 }
