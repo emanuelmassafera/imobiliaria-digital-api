db82ab98a626005b57cd537001b5c225084a2c00
Emanuel Massafera <emanuel301@live.com>
2021-05-08 14:07:11 -0300

    feat: create LoadPropertiesController

--- src/presentation/controllers/property/index.ts
+++ src/presentation/controllers/property/index.ts
@@ -1,2 +1,3 @@
 export * from './add-property-controller'
 export * from './load-owner-properties-controller'
+export * from './load-properties-controller'
---
+++ src/presentation/controllers/property/load-properties-controller.ts
@@ -0,0 +1,38 @@
+import { LoadProperties } from '@/domain/usecases'
+import { noContent, ok, serverError } from '@/presentation/helpers'
+import { Controller, HttpResponse } from '@/presentation/protocols'
+
+export class LoadPropertiesController implements Controller {
+  constructor (
+    private readonly loadProperties: LoadProperties
+  ) {}
+
+  async handle (request: LoadPropertiesController.Request): Promise<HttpResponse> {
+    try {
+      const properties = await this.loadProperties.loadProperties({
+        type: request.type,
+        availableTo: request.availableTo,
+        state: request.state,
+        city: request.city,
+        neighborhood: request.neighborhood,
+        minimumOfBedrooms: request.minimumOfBedrooms,
+        active: true
+      })
+
+      return properties.length ? ok(properties) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
+
+export namespace LoadPropertiesController {
+  export type Request = {
+    type?: string
+    availableTo?: string
+    state?: string
+    city?: string
+    neighborhood?: string
+    minimumOfBedrooms?: number
+  }
+}
